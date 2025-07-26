87556f17b72ed3010e6b2fd912f7beb409bc6fc2
MohHai <mohhai504@gmail.com>
2025-07-26 15:56:44 +0300

    Delete search.ts

--- search.ts
+++
@@ -1,7 +0,0 @@
-import { searchAmazonProducts } from './api/amazon';
-
-export async function globalProductSearch(query: string) {
-  const amazonResults = await searchAmazonProducts(query);
-  // You can add more integrations here (AliExpress, Shein, etc.)
-  return [...amazonResults];
-}
