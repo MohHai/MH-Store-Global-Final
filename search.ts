5b8bfe4ded42e87677c86fe3c972eadb38e075bf
MohHai <mohhai504@gmail.com>
2025-07-26 14:40:30 +0300

    Update search.ts

--- search.ts
+++ search.ts
@@ -1,1 +1,7 @@
-// search.ts content for scripts
+import { searchAmazonProducts } from './api/amazon';
+
+export async function globalProductSearch(query: string) {
+  const amazonResults = await searchAmazonProducts(query);
+  // You can add more integrations here (AliExpress, Shein, etc.)
+  return [...amazonResults];
+}
