2a5355ad8c131bb23eebcb96a8f900bbcbaeb3f9
MohHai <mohhai504@gmail.com>
2025-07-26 14:36:31 +0300

    Update amazon.ts

--- amazon.ts
+++ amazon.ts
@@ -1,1 +1,6 @@
-// amazon.ts content for lib
+import axios from 'axios';
+
+export async function searchAmazonProducts(query: string) {
+  const res = await axios.get(`/api/amazon?search=${encodeURIComponent(query)}`);
+  return res.data.products;
+}
