f35fefa91f5c7f49f405dc04f992d0921dec0e1e
MohHai <mohhai504@gmail.com>
2025-07-26 23:57:38 +0300

    Update tradalIntegration.ts

--- tradalIntegration.ts
+++ tradalIntegration.ts
@@ -1,6 +1,15 @@
 import axios from 'axios';
 
-export async function forwardWithTradal(orderData: any) {
-  const response = await axios.post('/api/tradal', orderData);
+export async function getTradalShippingEstimate(productLink: string, destinationCountry: string) {
+  const response = await axios.post('https://api.tradal.com/forwarding/estimate', {
+    productLink,
+    destinationCountry,
+  }, {
+    headers: {
+      'Authorization': `Bearer ${process.env.TRADAL_API_KEY}`,
+      'Content-Type': 'application/json',
+    },
+  });
+
   return response.data;
 }
