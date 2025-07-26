7288398bd47903f610e884379fc744e7b17941f0
MohHai <mohhai504@gmail.com>
2025-07-26 23:55:45 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,19 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["images.unsplash.com", "fakestoreapi.com"]
-  }
-};
+    domains: [
+      'm.media-amazon.com',
+      'cf.shopee.sg',
+      'ae01.alicdn.com',
+      'img.ltwebstatic.com',
+      'tradal.com',
+      'res.cloudinary.com'
+    ],
+  },
+  env: {
+    RAPIDAPI_KEY: process.env.RAPIDAPI_KEY,
+    AMAZON_API_KEY: process.env.AMAZON_API_KEY,
+  },
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
