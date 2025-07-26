d4b08bb9dc3538eb6271f142be16bcb838b79d33
MohHai <mohhai504@gmail.com>
2025-07-26 22:50:55 +0300

    Update amazon.ts

--- lib/sources/amazon.ts
+++ lib/sources/amazon.ts
@@ -1,18 +1,9 @@
+import axios from 'axios';
+
 export async function getAmazonProducts(query: string) {
-  return [
-    {
-      title: 'Amazon Echo Dot (5th Gen)',
-      price: '$49.99',
-      image: 'https://images-na.ssl-images-amazon.com/images/I/71xyz.jpg',
-      link: 'https://www.amazon.com/dp/B09B8V2KRW',
-      source: 'amazon',
-    },
-    {
-      title: 'Wireless Headphones - Noise Cancelling',
-      price: '$89.00',
-      image: 'https://images-na.ssl-images-amazon.com/images/I/81abc.jpg',
-      link: 'https://www.amazon.com/dp/B07XJ8C8F5',
-      source: 'amazon',
-    },
-  ];
+  const response = await axios.get('/api/amazon', {
+    params: { query },
+  });
+
+  return response.data;
 }
