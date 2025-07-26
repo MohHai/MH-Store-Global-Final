de25793b6c48f2916d5d93431d3b8b2c552084a4
MohHai <mohhai504@gmail.com>
2025-07-26 22:53:47 +0300

    Update shein.ts

--- lib/sources/shein.ts
+++ lib/sources/shein.ts
@@ -1,18 +1,9 @@
+import axios from 'axios';
+
 export async function getSheinProducts(query: string) {
-  return [
-    {
-      title: 'Summer Casual Floral Dress',
-      price: '$29.99',
-      image: 'https://img.shein.com/images/summer-dress.jpg',
-      link: 'https://www.shein.com/Summer-Casual-Floral-Dress-p-123456.html',
-      source: 'shein',
-    },
-    {
-      title: 'Men\'s Slim Fit Blazer',
-      price: '$44.00',
-      image: 'https://img.shein.com/images/mens-blazer.jpg',
-      link: 'https://www.shein.com/Mens-Slim-Fit-Blazer-p-789012.html',
-      source: 'shein',
-    },
-  ];
+  const response = await axios.get('/api/shein', {
+    params: { query },
+  });
+
+  return response.data;
 }
