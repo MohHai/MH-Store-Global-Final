c6dba2ac57b36504f5678d5702005bc212ea65c6
MohHai <mohhai504@gmail.com>
2025-07-26 13:57:23 +0300

    Update amazonIntegration.ts

--- amazonIntegration.ts
+++ amazonIntegration.ts
@@ -1,1 +1,6 @@
-// Real code for amazonIntegration.ts
+import axios from 'axios';
+
+export async function searchAmazon(query: string) {
+  const response = await axios.get(`/api/amazon?query=${query}`);
+  return response.data;
+}
