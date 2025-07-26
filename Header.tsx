add9d8f7c77528154f27bfdbea17beac52d75ae1
MohHai <mohhai504@gmail.com>
2025-07-26 13:54:23 +0300

    Update Header.tsx

--- Header.tsx
+++ Header.tsx
@@ -1,1 +1,7 @@
-// Real code for Header.tsx
+export default function Header() {
+  return (
+    <header className="bg-black text-white p-4 text-center text-xl font-bold">
+      M.H Store Global
+    </header>
+  );
+}
