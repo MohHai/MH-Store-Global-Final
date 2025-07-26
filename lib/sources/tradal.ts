e64450b177e3b51dc52fc70fdd4efd50b19a5d1c
MohHai <mohhai504@gmail.com>
2025-07-26 22:53:02 +0300

    Update tradal.ts

--- lib/sources/tradal.ts
+++ lib/sources/tradal.ts
@@ -1,18 +1,11 @@
-export async function getTradalProducts(query: string) {
-  return [
-    {
-      title: 'Smart Watch X9 - Fitness Tracker',
-      price: '$65.00',
-      image: 'https://tradal.com/images/smartwatch-x9.jpg',
-      link: 'https://tradal.com/product/x9',
-      source: 'tradal',
-    },
-    {
-      title: 'Portable Solar Charger 20000mAh',
-      price: '$39.95',
-      image: 'https://tradal.com/images/solar-charger.jpg',
-      link: 'https://tradal.com/product/solar-charger',
-      source: 'tradal',
-    },
-  ];
+import axios from 'axios';
+
+export async function getTradalShippingEstimate(productLink: string, destinationCountry: string, zipCode: string) {
+  const response = await axios.post('/api/tradal', {
+    productLink,
+    destinationCountry,
+    zipCode,
+  });
+
+  return response.data;
 }
