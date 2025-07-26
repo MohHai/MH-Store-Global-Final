8d1d232eb9e15852921b27d6866aab5d201239f2
MohHai <mohhai504@gmail.com>
2025-07-26 15:31:48 +0300

    Create products.ts

---
+++ lib/products.ts
@@ -0,0 +1,20 @@
+import { getAmazonProducts } from './sources/amazon';
+import { getTradalProducts } from './sources/tradal';
+import { getAliExpressProducts } from './sources/aliexpress';
+import { getSheinProducts } from './sources/shein';
+
+export async function searchAllProducts(query: string) {
+  try {
+    const [amazon, tradal, aliexpress, shein] = await Promise.all([
+      getAmazonProducts(query),
+      getTradalProducts(query),
+      getAliExpressProducts(query),
+      getSheinProducts(query),
+    ]);
+
+    return [...amazon, ...tradal, ...aliexpress, ...shein];
+  } catch (error) {
+    console.error('Product search error:', error);
+    return [];
+  }
+}
