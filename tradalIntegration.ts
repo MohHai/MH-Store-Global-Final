2cf99dd195cd67f268f9ad9aafe39ab0e2c5bef2
MohHai <mohhai504@gmail.com>
2025-07-26 13:57:57 +0300

    Update tradalIntegration.ts

--- tradalIntegration.ts
+++ tradalIntegration.ts
@@ -1,1 +1,6 @@
-// Real code for tradalIntegration.ts
+import axios from 'axios';
+
+export async function forwardWithTradal(orderData: any) {
+  const response = await axios.post('/api/tradal', orderData);
+  return response.data;
+}
