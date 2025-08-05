b7678b1ca56c03708073cf2d8536d26f8568d2e5
MohHai <mohhai504@gmail.com>
2025-08-05 22:11:55 +0300

    Update tradal.ts

--- lib/sources/tradal.ts
+++ lib/sources/tradal.ts
@@ -1,11 +1,27 @@
 import axios from 'axios';
 
-export async function getTradalShippingEstimate(productLink: string, destinationCountry: string, zipCode: string) {
-  const response = await axios.post('/api/tradal', {
-    productLink,
-    destinationCountry,
-    zipCode,
-  });
+export async function getTradalProducts(query: string) {
+  // تكامل عبر RapidAPI أو endpoint تجريبي
+  const options = {
+    method: 'GET',
+    url: 'https://tradal-products.p.rapidapi.com/search',
+    params: { query },
+    headers: {
+      'X-RapidAPI-Key': process.env.RAPIDAPI_KEY!,
+      'X-RapidAPI-Host': 'tradal-products.p.rapidapi.com'
+    },
+  };
 
-  return response.data;
+  try {
+    const res = await axios.request(options);
+    return res.data.products?.map((item: any) => ({
+      title: item.title,
+      price: item.price || 'Unavailable',
+      image: item.image,
+      link: item.link,
+      source: 'tradal'
+    })) || [];
+  } catch {
+    return [];
+  }
 }
